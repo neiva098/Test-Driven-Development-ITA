876968d02f52cdbdfb72ceac57aa9871e6363190
Cristiano <neivacristiano@yahoo.com.br>
2020-07-05 20:46:17 -0300

    pt 2

--- TDD_Pilha/src/Pilha/Pilha.java
+++ TDD_Pilha/src/Pilha/Pilha.java
@@ -2,28 +2,36 @@ package Pilha;
 
 public class Pilha {
 	
-	private Object elemento;
+	private Object[] elementos = new Object[10];
 	private int quantidade = 0;
 
 	public boolean estaVazia() {
 		// TODO Auto-generated method stub
-		return elemento == null;
+		return this.quantidade == 0;
 	}
 
 	public Integer tamanho() {
 		// TODO Auto-generated method stub
-		return quantidade;
+		return this.quantidade;
 	}
 
 	public void empilha(Object elemento) {
 		// TODO Auto-generated method stub
-		this.elemento = elemento;
+		this.elementos[quantidade] = elemento;
 		this.quantidade++;
 	}
 
 	public Object topo() {
 		// TODO Auto-generated method stub
-		return elemento;
+		return this.elementos[quantidade -1];
+	}
+
+	public Object desempilha() {
+		Object desempilhado = this.topo();
+		
+		this.quantidade--;
+		
+		return desempilhado;
 	}
 
 }
